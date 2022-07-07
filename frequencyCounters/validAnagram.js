51638d8f335b97e324594f313c5c0731831ae928
JaimeGSandoval <jaime.devs@gmail.com>
2022-07-07 14:52:58 -0700

    Complete validAnagram

---
+++ frequencyCounters/validAnagram.js
@@ -0,0 +1,18 @@
+function validAnagram(str1, str2) {
+    if(str1.length !== str2.length) return false;
+    const lookUp = {};
+    
+    for(let i = 0; i < str1.length; i++) {
+      const char = str1[i];
+      lookUp[char] ? lookUp[char] += 1: lookUp[char] = 1;
+    }
+    
+    for(let i = 0; i < str2.length; i++) {
+      const char = str2[i]
+      if(!lookUp[char]) return false;
+      else lookUp[char] -= 1;
+    }
+    
+  
+    return true;
+  }

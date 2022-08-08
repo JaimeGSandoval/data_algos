d7b5a8305b9e2b98bdc579d6e79704c12d61fcab
JaimeGSandoval <jaime.devs@gmail.com>
2022-08-07 17:35:54 -0700

    add another solution for sort string

--- strings/sort-string.js
+++ strings/sort-string.js
@@ -2,8 +2,8 @@
 
 let MAX_CHAR = 26;
 
-// function to print string in sorted order
 function sortString(str) {
+  let result = "";
   // Hash array to keep count of characters.
   // Initially count of all characters is
   // initialized to zero.
@@ -23,9 +23,40 @@ function sortString(str) {
   // characters
   for (let i = 0; i < MAX_CHAR; i++) {
     for (let j = 0; j < charCount[i]; j++) {
-      document.write(String.fromCharCode("a".charCodeAt() + i));
+      result += String.fromCharCode("a".charCodeAt() + i);
     }
   }
+
+  return result;
+}
+
+function sortString(str) {
+  // Hash array to keep count of characters.
+  let letters = new Array(MAX_CHAR);
+  let result = "";
+
+  for (let i = 0; i < MAX_CHAR; i++) {
+    letters[i] = 0;
+  }
+
+  // Traverse string and increment
+  // count of characters
+  for (let x = 0; x < str.length; x++) {
+    // 'a'-'a' will be 0, 'b'-'a' will be 1,
+    // so for location of character in count
+    // array we will do str[i]-'a'.
+    letters[str[x].charCodeAt(0) - "a".charCodeAt(0)]++;
+  }
+
+  // Traverse the hash array and print
+  // characters
+  for (let i = 0; i < MAX_CHAR; i++) {
+    for (let j = 0; j < letters[i]; j++) {
+      result += String.fromCharCode(i + "a".charCodeAt(0));
+    }
+  }
+
+  return result;
 }
 
 let s = "chidori";

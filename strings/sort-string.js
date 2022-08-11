d1a6cc13bee6fe9bb7bae11045fe2d85ce8880f9
JaimeGSandoval <jaime.devs@gmail.com>
2022-08-11 13:32:47 -0700

    Add notes for code solution and remove old solution

--- strings/sort-string.js
+++ strings/sort-string.js
@@ -3,61 +3,31 @@
 let MAX_CHAR = 26;
 
 function sortString(str) {
-  let result = "";
+  let result = '';
   // Hash array to keep count of characters.
   // Initially count of all characters is
   // initialized to zero.
-  let charCount = new Array(MAX_CHAR);
+  let charCount = new Array(MAX_CHAR); // each index represents a character in ASCII from 97 - 122 (which are all the lowercase characters in the alphabet)
   charCount.fill(0);
 
   // Traverse string and increment
   // count of characters
   for (let i = 0; i < str.length; i++) {
     // 'a'-'a' will be 0, 'b'-'a' will be 1,
-    // so for location of character in count
-    // array we will do str[i]-'a'.
-    charCount[str[i].charCodeAt() - "a".charCodeAt()]++;
+    // the result of str[i].charCodeAt() - "a".charCodeAt() will be the index which is stored in the charCode array. The index  will be used to determine the ascii value of the character when traversed below
+    charCount[str[i].charCodeAt() - 'a'.charCodeAt()]++;
   }
 
-  // Traverse the hash array and print
-  // characters
+  // Traverse the hash array and print characters
   for (let i = 0; i < MAX_CHAR; i++) {
     for (let j = 0; j < charCount[i]; j++) {
-      result += String.fromCharCode("a".charCodeAt() + i);
+      // use 'a'.charCodeAt + i because i will point to an index. at that index will be a character. Its index position relates to it value in ascii. Since "a" equals 97 in ascii, adding i to 97 will gives us the ascii value of the character at the index of i
+      result += String.fromCharCode('a'.charCodeAt() + i);
     }
   }
 
   return result;
 }
 
-function sortString(str) {
-  // Hash array to keep count of characters.
-  let letters = new Array(MAX_CHAR);
-  let result = "";
-
-  for (let i = 0; i < MAX_CHAR; i++) {
-    letters[i] = 0;
-  }
-
-  // Traverse string and increment
-  // count of characters
-  for (let x = 0; x < str.length; x++) {
-    // 'a'-'a' will be 0, 'b'-'a' will be 1,
-    // so for location of character in count
-    // array we will do str[i]-'a'.
-    letters[str[x].charCodeAt(0) - "a".charCodeAt(0)]++;
-  }
-
-  // Traverse the hash array and print
-  // characters
-  for (let i = 0; i < MAX_CHAR; i++) {
-    for (let j = 0; j < letters[i]; j++) {
-      result += String.fromCharCode(i + "a".charCodeAt(0));
-    }
-  }
-
-  return result;
-}
-
-let s = "chidori";
+let s = 'chidori';
 sortString(s);

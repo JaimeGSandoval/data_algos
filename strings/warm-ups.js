3d8b27f80b62f04088b0bf661e63992c92210d0a
JaimeGSandoval <jaime.devs@gmail.com>
2022-12-08 23:24:32 -0800

    complete sum strings problem

--- strings/warm-ups.js
+++ strings/warm-ups.js
@@ -97,3 +97,15 @@ function booleanToString(b) {
 function noSpace(x) {
   return x.replace(/\s/g, '');
 }
+
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+// Example: (Input1, Input2 -->Output)
+
+Notes:
+
+// If either input is an empty string, consider it as zero.
+
+// Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
+
+const sumStr = (a,b) => (Number(a) + Number(b)).toString();

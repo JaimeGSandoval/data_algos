fec1f4990aa9f88529520e39d97bafb6ee24ce2b
JaimeGSandoval <jaime.devs@gmail.com>
2022-12-13 19:52:49 -0800

    complete descending order problem

--- strings/warm-ups.js
+++ strings/warm-ups.js
@@ -109,3 +109,17 @@ Notes:
 // Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
 
 const sumStr = (a,b) => (Number(a) + Number(b)).toString();
+
+// Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+// Examples:
+// Input: 42145 Output: 54421
+
+// Input: 145263 Output: 654321
+
+// Input: 123456789 Output: 987654321
+
+// non-negative integer arg
+// return its digits in descending order
+
+const descendingOrder = (num) => Number(num.toString().split('').sort((a, b) => b - a).join(''));

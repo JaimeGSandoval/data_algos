2a625164ea58edb89220972b2385492059a708d8
JaimeGSandoval <jaime.devs@gmail.com>
2022-12-14 20:33:11 -0800

    complete two to one

--- strings/warm-ups.js
+++ strings/warm-ups.js
@@ -173,3 +173,20 @@ function printerError(s) {
 
   return `${totalErrors}/${s.length}`;
 }
+
+
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+
+function longest(s1, s2) {
+  let str = new Set([...s1, ...s2]);
+  return [...str].sort().join('');
+  }
+  

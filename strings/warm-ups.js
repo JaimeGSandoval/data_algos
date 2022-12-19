985450de673b1b731b762946549477fa591fef45
JaimeGSandoval <jaime.devs@gmail.com>
2022-12-18 20:33:35 -0800

    complete friends problem

--- strings/warm-ups.js
+++ strings/warm-ups.js
@@ -281,4 +281,16 @@ function highAndLow(numbers){
   return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
 }
 
-  
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+function friend(friends){
+  return friends.filter((friend) => {
+    if (friend.length === 4) return friend;
+  })
+}
+
+function friend(friends){
+  return friends.filter(friend => friend.length === 4)
+}
